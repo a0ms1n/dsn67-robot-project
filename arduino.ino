9511a82f6317ae898b911a49ff48ee2de8718289
a0ms1n <150027949+a0ms1n@users.noreply.github.com>
2025-01-22 11:33:24 +0700

    Update arduino.ino

--- arduino.ino
+++ arduino.ino
@@ -1,20 +1,45 @@
 //Install NewPing 
 #include <NewPing.h>
-
+#include <Servo.h>
+Servo servo;
 
 const int trigPin = 11;  
 const int echoPin = 10; 
+const int servoPin = 3;
+
+// Left
+const int M1_a = 4; //in1
+const int M1_b = 5; //in2
+
+// Right
+const int M2_a = 6; //in3
+const int M2_b = 7; //in4
+ 
 NewPing sonar(trigPin,echoPin, 200);
 
+void SetLeft(bool a,bool b){
+  digitalWrite(M1_a,a);
+  digitalWrite(M1_b,b);
+}
+
+void SetRight(bool a,bool b){
+  digitalWrite(M2_a,a);
+  digitalWrite(M2_b,b);
+}
 
 void setup() {
   pinMode(trigPin, OUTPUT);  
 	pinMode(echoPin, INPUT);  
 	Serial.begin(9600); 
-
+  servo.attach(servoPin);
+  servo.write(0);
+  delay(100);
+  pinMode(M1_a, OUTPUT); pinMode(M1_b, OUTPUT); pinMode(M2_a, OUTPUT); pinMode(M2_b, OUTPUT);
+  SetLeft(0,1);
+  SetRight(1,0);
 }
 
 void loop() {
-  Serial.print(sonar.ping_cm());
-  delay(10000);
+  Serial.println(sonar.ping_cm());
+  delay(100);
 }
